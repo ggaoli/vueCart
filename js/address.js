3da9514a5579e4f77d3646626542fcdddc367272
gaoli <ggaoli@hotmail.com>
2017-02-25 15:57:00 +0800

    v1.0

--- js/address.js
+++ js/address.js
@@ -1,8 +1,10 @@
 new Vue({
     el: '.container',
     data: {
-        limitNum:'3',
-        addressList: []
+        limitNum: '3',
+        addressList: [],
+        currentIndex: '',
+        shippingMethod:1
     },
     mounted: function () {
         this.$nextTick(function () {
@@ -23,8 +25,22 @@ new Vue({
                 }
             })
         },
-        loadMore:function () {
-            this.limitNum=this.addressList.length;
+        loadMore: function () {
+            if (this.limitNum != this.addressList.length) {
+                this.limitNum = this.addressList.length;
+            } else {
+                this.limitNum = 3;
+            }
+
+        },
+        setDefault: function (addressId) {
+            this.addressList.forEach(function (item, index) {
+                if (item.addressId == addressId) {
+                    item.isDefault = true;
+                } else {
+                    item.isDefault = false;
+                }
+            })
         }
     }
 });
